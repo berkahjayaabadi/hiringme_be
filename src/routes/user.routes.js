6d9e6058a47309f4dbb3d338f49d980b957c1d7a
Ajii <92772787+jiamudza@users.noreply.github.com>
2023-03-17 21:00:54 +0700

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express();
-const verifyToken = require("../helper/verifyToken");
 //import controller=
 const userController = require("../controller/user.controller");
 const formUpload = require("../helper/upload");
